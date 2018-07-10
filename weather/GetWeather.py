fa46f37af24b6d658af56460ca694f8ec8a1c824
milivi <32281258+milivi@users.noreply.github.com>
2018-07-10 08:06:20 -0400

    Feature change location (#8)
    * Weather location change functions
    
    The weather location change works on a basic level.  Upon right click and selection it brings up a window that allows the user to enter a city and the weather location is changed to that city.
    
    * change_location module
    
    A separate module created with change_location class to improve readability in GetWeather.py
    
    * Refactored Location Change
    
    Extracted location change functions to a separate module to improve readability.
    The change location window now closes itself after a new location is entered.
    
    * Rename getWeather.py to GetWeather.py
    
    * Restructure folder
    
    * Ignore __pycache__
    
    * .gitignore update
    
    Add .pyc to .gitignore file.
    
    * Allow country change
    
    Add entry field for country.
    Also add warning when an invalid city is entered.
    
    * Continued debugging of change location
    
    Committing progress to be able to work on it from another computer....
    
    * Add drag ability
    
    Add the ability to drag the change location window.
    
    * Print Invalid Entry once
    
    Stopped 'Invalid Entry' error from printing multiple times.
    Also remove .txt from .gitignore
    
    * Enhance Location GUI
    
    Close any open change location window on exit of the label.
    Add labels to location window.
    
    * Pass Location Objects
    
    Pass location object rather than just IDs to be able to access the place name for the locations.
    
    * Adjust Window location
    
    Adjust window location to pop up a little above the cursor location
    
    * Fix Formatting

---
+++ weather/GetWeather.py
@@ -0,0 +1,117 @@
+"""GetWeather.py gets the weather from OpenWeatherMap.
+
+It puts the temperature in a small draggable label in the lower left of the screen.
+"""
+
+import time
+import math
+import tkinter
+import pyowm
+import change_location as wcl
+
+class temperature:
+	def __init__ (self, place):
+		"""Constructor for temperature class.
+
+		Parameter: place - the location to search.
+		"""
+		self.place = place
+		
+		self.label = tkinter.Label(text=':(', font=('Courier','20'), 
+					fg='black', bg='grey', 
+					height=2, width=3)
+		self.label.master.overrideredirect(True)
+		self.label.master.wm_attributes("-topmost", True)
+		self.label.master.geometry("+5+850")
+		self.label.master.wm_attributes("-transparentcolor", "grey")
+		self.label.bind("<Button-1>", self.click)
+		self.label.bind("<Button-3>", self.right_click)
+		self.label.bind("<B1-Motion>", self.drag)
+		self.label.pack()
+		
+		self.popup_menu = tkinter.Menu(self.label.master)
+		self.popup_menu.add_command(label="Switch Color", command=self.change_color)
+		self.popup_menu.add_command(label="Change Location", command=self.get_location)
+		self.popup_menu.add_command(label="Exit", command=self.destroy)
+		
+		self.update_temp()
+
+	def update_temp(self):
+		"""Update the temperature every 10 minutes between 7 AM and 5 PM"""
+		cur_hour = time.localtime()[3]
+		if cur_hour > 6 and cur_hour < 17:
+			self.observation = owm.weather_at_id(self.place)
+			w = self.observation.get_weather()
+			temp = math.ceil(w.get_temperature('fahrenheit')['temp'])
+			ftemp = f'{temp}' + u'\N{DEGREE SIGN}'
+			self.label.configure(text=ftemp)
+		self.timer = self.label.after(600000, self.update_temp)
+	
+	def get_pointer_x(self):
+		"""Return the x-coordinate of the cursor position."""
+		return self.label.master.winfo_pointerx()
+		
+	def get_pointer_y(self):
+		"""Return the y-coordinate of the cursor position."""
+		return self.label.master.winfo_pointery()
+
+	def update_position(self, x, y):
+		"""Update the position of the label.
+		
+		Parameters: x - x-coordinate to move to
+			    y - y-coordinate to move to
+		"""
+		self.label.master.geometry(f'+{x}+{y}')
+		
+	def click(self, event):
+		"""On the left click event, saves the coordinates of the event."""
+		self.offset_x = event.x
+		self.offset_y = event.y
+	
+	def drag(self, event):
+		"""On the drag event, calculates and updates the labels position."""
+		x = self.get_pointer_x() - self.offset_x 
+		y = self.get_pointer_y() - self.offset_y
+		self.update_position(x, y)
+
+	def right_click(self, event):
+		"""On right click, popup menu appears."""
+		try:
+			self.popup_menu.tk_popup(event.x_root, event.y_root, 0)
+		finally:
+			self.popup_menu.grab_release()
+			
+	def change_color(self):
+		"""Switches the color of the label between light and dark."""
+		if self.label.cget('fg') == 'black':
+			font = 'white'
+			backing = 'lavender blush'
+		else:
+			font = 'black'
+			backing = 'grey'
+		self.label.config(fg=font, bg=backing)
+		self.label.master.wm_attributes("-transparentcolor", backing)
+		
+	def get_location(self):
+		"""Get the location change input by user and update the temperature."""
+		place_location = self.observation.get_location()
+		self.change_loc = wcl.change_location(owm, place_location, self.get_pointer_x, self.get_pointer_y)
+		place_location = self.change_loc.get_location()
+		self.place = place_location.get_ID()
+		self.label.after_cancel(self.timer)
+		try:
+			self.update_temp()
+		except tkinter.TclError:
+			pass
+		
+	def destroy(self):
+		"""Destroy any windows remaining open."""
+		self.change_loc.destroy()
+		self.label.master.destroy()
+
+owm = pyowm.OWM('2e87feb9a967628ae1d395b6c0d26cab')
+place = 5129780
+
+widg = temperature(place)
+widg.label.mainloop()
+
---
+++ weather/change_location.py
@@ -0,0 +1,90 @@
+"""change_location.py gets a user entered location to update the weather location
+
+The location's validity is checked through pyowm"""
+
+import tkinter
+import pyowm
+
+class change_location:
+	def __init__ (self, owm, cur_location, x_coordinate, y_coordinate):
+		"""Constructor for change_location class.
+		
+		Creates window to change the weather location.
+		Parameters: owm - Open Weather Map Object
+			    cur_location - the current weather location"""
+		self.location_win = tkinter.Tk()
+		self.location_win.overrideredirect(True)
+		self.location_win.geometry("200x100")
+		self.location_win.geometry("+5+750")
+		self.location_win.title("Get Outta Town")
+		self.location_win.bind("<Button-1>", self.click)
+		self.location_win.bind("<B1-Motion>", self.drag)
+		
+		tkinter.Label(self.location_win, text='City').grid(row=0, column=0, pady=10)
+		self.city_entry = tkinter.Entry(self.location_win)
+		self.city_entry.insert(0, cur_location.get_name())
+		self.city_entry.grid(row=0, column=1, pady=10)
+		
+		tkinter.Label(self.location_win, text='Country').grid(row=1, column=0)
+		self.country_entry = tkinter.Entry(self.location_win)
+		self.country_entry.insert(0, "US")
+		self.country_entry.grid(row=1, column=1)
+		
+		self.b = tkinter.Button(self.location_win, text="Enter", 
+							command=lambda: 
+								self.check_location(self.city_entry.get(), 
+												self.country_entry.get().upper()))
+		self.b.grid(row=2, column=1, pady=10)
+		
+		self.invalid = tkinter.Label(self.location_win, 
+								text="Invalid City, try again", fg="red")
+		
+		self.owm = owm
+		self.new_location = cur_location
+		self.x_coordinate = x_coordinate()
+		self.y_coordinate = y_coordinate()
+		
+	def get_location(self):
+		"""Get the location change from the user"""
+		self.location_win.geometry(f'+{self.x_coordinate}+{self.y_coordinate-150}')
+		self.location_win.mainloop()
+		self.destroy()
+		return self.new_location
+
+	def check_location(self, new_location, new_country):
+		"""Checks the entered weather location and updates if it is valid."""
+		registry = self.owm.city_id_registry()
+		try:
+			poss_locations = registry.locations_for(new_location, 
+												country=new_country, 
+												matching='nocase')
+		except(ValueError):
+			self.invalid_entry()
+		else:
+			if poss_locations:
+				self.new_location = poss_locations[0]
+				self.location_win.quit()
+			else:
+				self.invalid_entry()
+		
+	def destroy(self):
+		"""Close the window."""
+		try:
+			self.location_win.destroy()
+		except tkinter.TclError as error:
+			pass
+	
+	def invalid_entry(self):
+		"""Warn that the entered city is not a valid location."""
+		self.invalid.pack()
+		
+	def click(self, event):
+		"""On the left click event, saves the coordinates of the event."""
+		self.offset_x = event.x
+		self.offset_y = event.y
+	
+	def drag(self, event):
+		"""On the drag event, calculates and updates the labels position."""
+		x = self.location_win.winfo_pointerx() - self.offset_x 
+		y = self.location_win.winfo_pointery() - self.offset_y
+		self.location_win.geometry(f'+{x}+{y}')
